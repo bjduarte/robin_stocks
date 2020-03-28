43e039bbd9059bc184284a97929ff8d573ca857e
RedEyeDragon <bjduarte@asu.edu>
2020-03-28 08:48:31 -0700

    added functionality for adding position, removing position, removing holding, displaying holdings, displaying menu options, exception handling. todo, raise exception for symb, test exception handling further.

--- myHoldings.py
+++ myHoldings.py
@@ -13,33 +13,87 @@ from itertools import zip_longest
 class MyHoldings:
 
   def __init__(self):
-    # self.holdings = []
-    # self.batch = ()
-    self.myHoldings = {}
-    self.stockData = {}
+    self.stockData = {} # dictionary for working with positions
+    self.myHoldings = {} # dictionary for displaying positions
 
 
+  def readData(self):
+    # reading in current data
+    for file in os.listdir('.'):
+      if fnmatch.fnmatch(file, 'stockData.json'):
+        f = open(file, 'r')
+        self.stockData = json.load(f)
+        f.close()
+
+    for file in os.listdir('.'):
+      if fnmatch.fnmatch(file, 'myHoldings.json'):
+        f = open(file, 'r')
+        self.myHoldings = json.load(f)
+        f.close()
+
+
+  # Adds a position to holdings
   def addPosition(self, symb, quanity, price):
-    batch = (quanity, price)
-    holdings = self.myHoldings.get(symb)
+    self.readData()
+    lot = (quanity, price)
+    position = self.stockData.get(symb)
 
-    if symb in self.myHoldings:
-      holdings.append(batch)
-      self.myHoldings[symb] = holdings
-      # print('Updating: ', batch)
-      # print('Current: ', self.myHoldings.get(symb))
+    # check to see if position currently exists
+    # append if exists, create new if not
+    if symb in self.stockData:
+      print(symb, ': found! Adding lot to current holding...')
+      position.append(lot)
+      self.stockData[symb] = position
+      print(self.stockData)
     else:
-      self.myHoldings[symb] = [batch]
-      # print('Adding: ', batch)
-      # print('Current: ', self.myHoldings.get(symb))
+      print(symb, ': Not found! Creating new holding...')
+      self.stockData[symb] = [lot]
+      print(self.stockData)
 
 
+  # Removes a position from holdings
+  def removeHolding(self, symb):
+    self.readData()
+    print('Searching your holdings for: ', symb)
+
+    # check to see if position exists
+    if symb in self.stockData:
+      print(symb, '- found! removing...')
+      del self.stockData[symb]
+      print(symb, ': Holding removed!\n', self.stockData)
+    else:
+      print(symb, ': Position not found!')
+
+
+    # remove a lot from position
+  def removePosition(self, symb, quanity, price):
+    self.readData()
+    lot = [quanity, price]
+    position = self.stockData.get(symb)
+    removeLot = position.index(lot)
+    print('Searching your holdings for: ', symb)
+
+    # check for symbol in holdings
+    if symb in self.stockData:
+      print(symb, '- found!')
+
+      position.pop(removeLot)
+      self.stockData[symb] = position
+      print(symb, 'position updated: \n', position)
+
+    else:
+      print(symb, 'Symbol not found!')
+      
+    return len(position)
+
+
+  # calculates the net price of each position
   def netPrice(self):
-    holdings = self.myHoldings.values()
+    position = self.stockData.values()
+    lots = [i for i in position]
     netPrice = []
-    lots = [i for i in holdings]
 
-    for lot in holdings:
+    for lot in position:
       # split shares into individual list
       getShares = [j[0] for j in lot]
 
@@ -57,65 +111,127 @@ class MyHoldings:
 
       # calculating the netPrice of each stock holding
       netPrice.append((totalPrice / numOfShares))
-      # print('netprice: ', netPrice)
 
-    # building the dictionary to be written
-    symb = [i for i in self.myHoldings.keys()]
-    self.stockData = {'ticker': symb, 'lot': lots, 'netPrice': netPrice}
+    # building a dictionary for pretty printing
+    symb = [i for i in self.stockData.keys()]
+    self.myHoldings = {'ticker': symb, 'lot': lots, 'netPrice': netPrice}
 
 
+
+  # writes both dictionaries to json file
+  # myHoldings.json contains dict for pretty printing
+  # stockData.json contains bict for operations
   def writeData(self):
     with open('myHoldings.json', 'w') as f:
+      json.dump(self.myHoldings, f)
+    f.close()
+
+    with open('stockData.json', 'w') as f:
       json.dump(self.stockData, f)
     f.close()
 
 
-# format stock data into human readable text
+  # write stock data into a human readable format
   def formatData(self):
-    for file in os.listdir('.'):
-      if fnmatch.fnmatch(file, 'myHoldings.json'):
-        f = open(file, 'r')
-        fin = json.load(f)
-        f.close()
+    self.readData()
+    fin = self.myHoldings
 
-        line1 = zip_longest(fin.get('ticker'), fin.get('lot'), fin.get('netPrice'), fillvalue=' - ')
-        fout = open('myHoldings.txt', "w")
-        fout.write('Stock Ticker: \t|\t Lot: \t|\t Net Price: \t\n')
-        for data in line1:
-          fout.write(str(data) + "\n")
-        fout.close()
+    dataLine = zip_longest(fin.get('ticker'), fin.get('lot'), fin.get('netPrice'), fillvalue=' - ')
+    fout = open('myHoldings.txt', "w")
+    fout.write('Position \t|\t Lot: \t|\t Net Price: \t\n')
+    for data in dataLine:
+      fout.write(str(data) + "\n")
+    fout.close()
+
+
+  def displayHoldings(self):
+    self.readData()
+    fin = self.myHoldings
+
+    if len(fin) != 0:
+      dataLine = zip_longest(fin.get('ticker'), fin.get('lot'), fin.get('netPrice'), fillvalue=' - ')
+      print('Position \t|\t Lot: \t|\t Net Price: \t\n')
+      for data in dataLine:
+        print(str(data) + "")
+    else:
+      print('No holdings found!')
+
+
+  def displayMenu(self):
+    print('Make your selection: ')
+    selection = input('1. Add position \n 2. Remove position \n 3. Update position \n 4. Display holdings \n 5. Exit\n')
+
+    return selection
 
 
 if __name__ == "__main__":
   mh = MyHoldings()
 
-  flag = True
-  print("Enter your position: ")
-  while flag:
-    try:
-      symb = input("Enter stock ticker: ")
-      quanity = int(input('Enter the number of shares: '))
-      price = float(input('Enter the purchase price: '))
-      mh.addPosition(symb, quanity, price)
-    except ValueError:
-      print('Sorry! Invalid value! Please re-enter your position')
-      symb = input("Enter stock ticker: ")
-      quanity = int(input('Enter the number of shares: '))
-      price = float(input('Enter the purchase price: '))
+
+  option = mh.displayMenu()
+  while True:
+    if option == '1':
+      while True:
+        try:
+          symb = input("Enter stock ticker: ").upper()
+          quanity = int(input('Enter the number of shares: '))
+          price = float(input('Enter the purchase price: '))
+
+          mh.addPosition(symb, quanity, price)
+          mh.netPrice()
+          print('Writing data')
+          mh.writeData()
+          print('Formating data')
+          mh.formatData()
+          break
+
+        except ValueError as ve:
+          print('Error! ', type(ve), ': ', ve)
+
+    elif option == '2':
+      while True:
+        try:
+          symb = input("Enter stock ticker: ").upper()
+          mh.removeHolding(symb)
+          mh.netPrice()
+          mh.writeData()
+          break
+
+        except ValueError as ve:
+          print('Error! ', type(ve), ': ', ve)
+
+    elif option == '3':
+      while True:
+        try:
+          symb = input("Enter stock ticker: ").upper()
+          sd = mh.stockData.get(symb)
+          print('Here is your current holding: ', sd)
+          
+          quanity = int(input('Enter quanity to remove'))
+          price = float(input('Enter price to remove'))
+          position = mh.removePosition(symb, quanity, price)
+          if position == 0:
+            print('Holding is empty, removing holding!')
+            mh.removeHolding(symb)
+            mh.writeData()
+            break
+          else:
+            mh.netPrice()
+            mh.writeData()
+            break
+
+        except ValueError as ve:
+          print('error: ', type(ve), ve)
+
+    elif option == '4':
+      mh.displayHoldings()
+    elif option == '5':
+      print('Exiting...')
+      print("Done!")
+      break
     else:
-      exit = input("Enter another? (y or n)")
-      if exit == 'n':
-        flag = False
-      else:
-        continue
+      print(option, "is an invalid option! Enter 1, 2, 3, 4, or 5\n")
 
-      mh.netPrice()
-  print('Writing data')
-  mh.writeData()
-
-  print('Formating data')
-  mh.formatData()
-
-  print("Done!")
+    option = mh.displayMenu()
 
 
